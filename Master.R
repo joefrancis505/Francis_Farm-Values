66cf27af3a6881107db2f0f607aefd9916dd0c47
joefrancis505 <joefrancis505@gmail.com>
2025-06-10 21:32:12 +0100

    v4

--- Master.R
+++ Master.R
@@ -6,7 +6,7 @@ cat("\014")
 
 source("Download_IPUMS.R")
 source("RDD_database_v4.R")
-source("Event_study_database_v4.R")
+source("Event_study_database_v3.R")
 source("Event_study_normalization_v2.R")
 source("Figure_1.R")
 source("Event_study_v4.R")
